ccfdfc72be571cbc19783ec6c1c6997069215379
Fabián Bustos <121578605+fbustos-o@users.noreply.github.com>
2025-06-16 11:00:29 -0400

    Delete GEV directory

--- GEV/apps/mi_casa_eficiente/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def IndexView(request):
-    '''
-    Función que retorna un mensaje de bienvenida.    
-    '''
-    return render(request, "index.html")
